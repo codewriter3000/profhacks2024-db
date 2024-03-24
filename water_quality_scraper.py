aeb00315abbc11ea5692342afaabc7af542498ce
amicharski <amicharski@outlook.com>
2024-03-24 01:16:42 -0400

    hallelujah

--- water_quality_scraper.py
+++ water_quality_scraper.py
@@ -1,10 +1,14 @@
-import functools
+import csv_to_sqlite
 
-sqlqueries = """
-PRAGMA foreign_keys = ON;
+options = csv_to_sqlite.CsvOptions(typing_style="full", encoding="windows-1250")
+input_files = ["NJDEP_Beach_Data_2024_03_23.csv"]  # pass in a list of CSV files
+csv_to_sqlite.write_csv(input_files, "beach_data.sqlite", options)
 
-CREATE TABLE nj_water_tests (
-    municipality_code INTEGER,
-    FOREIGN KEY(municipality_code) REFERENCES nj_municipalities(municipality_code)
-);
-"""
+# sqlqueries = """
+# PRAGMA foreign_keys = ON;
+#
+# CREATE TABLE nj_water_tests (
+#     municipality_code INTEGER,
+#     FOREIGN KEY(municipality_code) REFERENCES nj_municipalities(municipality_code)
+# );
+# """
